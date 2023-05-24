ce2300c11d249c64aa7767929ea37b816d4e5d4a
tomi <tomi.koski@gmail.com>
2023-05-24 22:12:15 +0300

    rs485 something

---
+++ rs485/rs485-open.py
@@ -0,0 +1,35 @@
+import serial
+from time import sleep
+'''
+Full open command: 8A 01 00 11 9A
+
+8A 01 01 11 9B Open channel 1             8A 01 02 11 98 Open channel 2
+8A 01 03 11 99 Open channel 3             8A 01 04 11 9E Open channel 4
+8A 01 05 11 9F Open channel 5             8A 01 06 11 9C Open channel 6
+8A 01 07 11 9D Open channel 7             8A 01 08 11 92 Open channel 8
+8A 01 09 11 93 Open channel 9             8A 01 0A 11 90 Open channel 10
+8A 01 0B 11 91 Open channel 11            8A 01 0C 11 96 Open channel 12
+8A 01 0D 11 97 Open channel 13            8A 01 0E 11 94 Open channel 14
+8A 01 0F 11 95 Open channel 15            8A 01 10 11 8A Open channel 16
+8A 01 11 11 8B Open channel 17            8A 01 12 11 88 Open channel 18
+8A 01 13 11 89 Open channel 19            8A 01 14 11 8E Open channel 20
+8A 01 15 11 8F Open channel 21            8A 01 16 11 8C Open channel 22
+8A 01 17 11 8D Open channel 23            8A 01 18 11 82 Open channel 24
+'''
+
+try:
+  s = serial.Serial(port='/dev/ttyUSB0',baudrate=9600, timeout=3.0)
+  s.write(bytes.fromhex("8a0101119b"))
+  sleep(2)
+  s.write(bytes.fromhex("8a01021198"))
+  sleep(2)
+  s.write(bytes.fromhex("8a01031199"))
+  sleep(4)
+  #s.write(bytes.fromhex("8A 01 12 11 88"))
+  #open all
+  s.write(bytes.fromhex("8A0100119a"))
+except:
+  print("error!")
+
+finally:
+  s.close()
