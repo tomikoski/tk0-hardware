eef1c38a7eac738433951f8eef56a8d464b10f7e
Tomi <tomi.koski@gmail.com>
2023-12-25 15:27:41 +0200

    voltage glitch skeletonS, none working yet :)

--- glitching/chipwhisperer/voltage_glitch1.1.py
+++ glitching/chipwhisperer/voltage_glitch1.1.py
@@ -11,6 +11,7 @@ SCOPETYPE = 'OPENADC'
 PLATFORM = 'CWNANO'
 #SS_VER = 'SS_VER_2_1' #REMOVE DUE SERIAL2 read errors
 SS_VER = 'SS_VER_1_1'
+MAX_SUCCESSES = 1
 
 def reset_target(scope):
     if PLATFORM == "CW303" or PLATFORM == "CWLITEXMEGA":
@@ -114,12 +115,10 @@ elif PLATFORM == "CW308_STM32F3":
 #TOMI
 elif PLATFORM == "CWNANO":
     scope.vglitch_setup(glitcht=None, default_setup=True) # repeat = 1 ext_offset = 0
-    #gc.set_range("ext_offset", 0, 10)
-    #gc.set_range("width", -50, 50)
-    #gc.set_range("offset", -48.8, 48.8)
     gc.set_range("ext_offset", 9, 12)
     gc.set_range("width", 47.6, 49.6)
     gc.set_range("offset", -19, -21.5)
+    gc.set_global_step(0.4)
     #gc.set_range("width", 34.7, 36)
     #gc.set_range("offset", -41, -30)
     #gc.set_range("ext_offset", 6, 6)
@@ -132,74 +131,62 @@ if(len(sys.argv) > 1):
 print(f"REPEAT: {scope.glitch.repeat}")
     
 
-# "glitch_only" - insert a glitch for a portion of a clock cycle based 
-#                 on scope.glitch.width and scope.glitch.offset
-# enable_only" - insert a glitch for an entire clock cycle
-#scope.glitch.output = "glitch_only"
-#scope.glitch.output = "enable_only"
-
-
-scope.adc.timeout = 0.1
+scope.adc.timeout = 0.5
 
 reboot_flush()
-sample_size = 1
-#loff = scope.glitch.offset
-#lwid = scope.glitch.width
 total_successes = 0
+successes = 0
+resets = 0
 for glitch_setting in gc.glitch_values():
     scope.glitch.offset = glitch_setting[1]
     scope.glitch.width = glitch_setting[0]
     scope.glitch.ext_offset = glitch_setting[2]
+
+    # break out
+    total_successes += successes
     successes = 0
     resets = 0
-    for i in range(10):
-        print(f"WIDTH: {scope.glitch.width}, OFFSET: {scope.glitch.offset}, EXT_OFFSET: {scope.glitch.ext_offset}")
-        target.flush()
+    if total_successes > MAX_SUCCESSES:
+       break
 
-        scope.arm()
+    print(f"WIDTH: {scope.glitch.width}, OFFSET: {scope.glitch.offset}, EXT_OFFSET: {scope.glitch.ext_offset}")
+    target.flush()
+    scope.arm()
 
-        #Do glitch loop
-        target.write("g\n")
+    #Do glitch loop
+    target.simpleserial_write("g", bytearray([]))
 
-        ret = scope.capture()
-        print("ret {}".format(ret))
-        val = target.simpleserial_read_witherrors('r', 4, glitch_timeout=10) #For loop check
-        #print(val)
-        
-        scope.io.glitch_hp = False
-        scope.io.glitch_hp = True
-        scope.io.glitch_lp = False
-        scope.io.glitch_lp = True
-        
-        if ret:
-            print('Timeout - no trigger')
+    ret = scope.capture()
+
+    if ret:
+        #print('Timeout - no trigger')
+        gc.add("reset")
+        resets += 1
+
+        #Device is slow to boot?
+        reboot_flush()
+
+    else:
+        val = target.simpleserial_read_witherrors('r', 4, glitch_timeout=10, timeout=50)#For loop check
+        if val['valid'] is False:
             gc.add("reset")
-            resets += 1
-            print(val)
-
-            #Device is slow to boot?
             reboot_flush()
-
+            resets += 1
+            #print(val)
         else:
-            if val['valid'] is False:
-                gc.add("reset")
-                reboot_flush()
-                resets += 1
-                print(val)
-            else:
-                gcnt = struct.unpack("<I", val['payload'])[0]
+            gcnt = struct.unpack("<I", val['payload'])[0]
 
-                if gcnt != 2500: #for loop check
-                    print((scope.glitch.width, scope.glitch.offset, scope.glitch.ext_offset))
-                    gc.add("success")
-                    successes += 1
-                else:
-                    gc.add("normal")
-                    print("normal")
-    if successes > 0:
+            if gcnt != 2500: #for loop check
+                gc.add("success")
+                print((scope.glitch.width, scope.glitch.offset, scope.glitch.ext_offset))
+                successes += 1
+            else:
+                gc.add("normal")
+    if (successes > 0):
         print("successes = {}, resets = {}, offset = {}, width = {}, ext_offset = {}".format(successes, resets, scope.glitch.offset, scope.glitch.width, scope.glitch.ext_offset))
         total_successes += successes
-
 print("Done glitching")
 print("successes = {}, resets = {}, offset = {}, width = {}, ext_offset = {}".format(successes, resets, scope.glitch.offset, scope.glitch.width, scope.glitch.ext_offset))
+
+#enable logging
 cw.set_all_log_levels(cw.logging.WARNING)
