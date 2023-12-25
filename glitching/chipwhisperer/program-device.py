7bda09f529590c5ce2d7428be25c5da0d1e9b927
Tomi <tomi.koski@gmail.com>
2023-12-25 16:30:55 +0200

    program device parameters

--- glitching/chipwhisperer/program-device.py
+++ glitching/chipwhisperer/program-device.py
@@ -1,10 +1,9 @@
 import chipwhisperer as cw
-import os
+import sys
 
 scope = cw.scope()
-HOME = os.path.expanduser('~')
-
-#cw.program_target(scope, cw.programmers.STM32FProgrammer, HOME + "/git/chipwhisperer/hardware/victims/firmware/simpleserial-base/simpleserial-base-CWNANO.hex")
-cw.program_target(scope, cw.programmers.STM32FProgrammer, HOME + "/git/chipwhisperer/hardware/victims/firmware/simpleserial-glitch/simpleserial-glitch-CWNANO.hex")
+#cw.program_target(scope, cw.programmers.STM32FProgrammer, "./git/chipwhisperer/hardware/victims/firmware/simpleserial-base/simpleserial-base-CWNANO.hex")
+#cw.program_target(scope, cw.programmers.STM32FProgrammer, "./git/chipwhisperer/hardware/victims/firmware/simpleserial-glitch/simpleserial-glitch-CWNANO.hex")
+cw.program_target(scope, cw.programmers.STM32FProgrammer, sys.argv[1])
 
 scope.dis()
